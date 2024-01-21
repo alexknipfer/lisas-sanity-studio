4a0c971afd33240501f3d75904fe965b2023a142
Alexander Knipfer <alexanderknipfer@gmail.com>
2024-01-21 17:23:21 -0500

    Add inline svg

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,7 +1,9 @@
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemas'
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+import { visionTool } from '@sanity/vision'
+import { inlineSvgInput } from '@focus-reactive/sanity-plugin-inline-svg-input'
+
+import { schemaTypes } from './schemas'
 
 export default defineConfig({
   name: 'default',
@@ -10,7 +12,7 @@ export default defineConfig({
   projectId: 'tqyrdkqd',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [structureTool(), visionTool(), inlineSvgInput()],
 
   schema: {
     types: schemaTypes,
