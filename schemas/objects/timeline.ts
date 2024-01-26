585d1f7902057373b6977e09c1d4f44aa75b8c6b
Alexander Knipfer <alexanderknipfer@gmail.com>
2024-01-25 21:27:52 -0500

    Add timeline item icon

--- schemas/objects/timeline.ts
+++ schemas/objects/timeline.ts
@@ -6,6 +6,11 @@ export const timeline = defineType({
   type: 'object',
   fields: [
     defineField({
+      name: 'timelineItemIcon',
+      description: 'Icon should be 1.5px stroke width and 24px in size',
+      type: 'inlineSvg',
+    }),
+    defineField({
       name: 'timelineYears',
       title: 'Timeline Years',
       type: 'array',
