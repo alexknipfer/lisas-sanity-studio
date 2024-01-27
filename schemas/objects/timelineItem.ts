2a8b80bef78ba4e474fdb3448c7ee2b202ef6672
Alexander Knipfer <alexanderknipfer@gmail.com>
2024-01-26 23:31:52 -0500

    Add image to timeline item

--- schemas/objects/timelineItem.ts
+++ schemas/objects/timelineItem.ts
@@ -9,11 +9,17 @@ export const timelineItem = defineType({
       name: 'name',
       title: 'Name',
       type: 'string',
+      validation: (rule) => rule.required(),
     }),
     defineField({
       name: 'description',
       title: 'Description',
       type: 'text',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'image',
+      type: 'image',
     }),
   ],
 })
